eab3bfb4dab767f8f7097456314b35c748e8d6a8
Diegofct <96diegofct@gmail.com>
2026-02-03 11:47:36 -0500

    Se crea la estructura del proyecto para desarrollar los componentes dentro de la carpeta components y en este caso se ha desarrollado el componente sidebar

---
+++ src/app/pages/Licitaciones/licitaciones/licitaciones.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-licitaciones',
+  imports: [],
+  templateUrl: './licitaciones.html',
+})
+export class Licitaciones {
+
+}
